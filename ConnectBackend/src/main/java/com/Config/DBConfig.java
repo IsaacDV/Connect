7c4158232e61d36a8f5429cbc3b204bb0e62df16
Isaac <Isaac@IsaacDV>
2017-12-05 13:33:32 +0530

    ojdbc

---
+++ ConnectBackend/src/main/java/com/Config/DBConfig.java
@@ -0,0 +1,12 @@
+package com.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {
+	
+	
+
+}
